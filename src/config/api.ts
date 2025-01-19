3fe9e982c9210ec2410c3aa5d7f03c1f24bb6138
duskalor <duskalor1@gmail.com>
2025-01-19 12:39:11 -0500

    first commit

---
+++ src/config/api.ts
@@ -0,0 +1,66 @@
+import fs from 'fs/promises';
+import { status } from '..';
+
+export const api = {
+  writeTasks: async (tasks: any) =>
+    await fs.writeFile('./DB.json', JSON.stringify({ tasks }, null, 2)),
+
+  getTasks: async (status?: status) => {
+    const data = await fs.readFile('./DB.json');
+    const { tasks } = JSON.parse(data.toString('utf8'));
+    return status ? tasks.filter((t: any) => t.status === status) : tasks;
+  },
+
+  getTaskbyId: async (id: number) => {
+    const tasks = await api.getTasks();
+    return tasks.find((t: any) => t.id === id);
+  },
+
+  addTask: async (task: any) => {
+    try {
+      const tasks = await api.getTasks();
+      tasks.push({
+        task,
+        id: tasks.length + 1,
+        status: status.todo,
+        createAt: new Date(),
+        updateAt: new Date(),
+      });
+      await api.writeTasks(tasks);
+    } catch (error) {
+      await api.writeTasks([
+        {
+          task,
+          id: 1,
+          status: status.todo,
+          createAt: new Date(),
+          updateAt: new Date(),
+        },
+      ]);
+    }
+  },
+
+  updateTask: async (id: string, task: any) => {
+    const tasks = await api.getTasks();
+    const indexTask = tasks.findIndex((v: any) => v.id === +id);
+    const newTasks = tasks.with(indexTask, {
+      ...tasks[indexTask],
+      task,
+      updateAt: new Date(),
+    });
+    await api.writeTasks(newTasks);
+  },
+  deleteTask: async (id: string) => {
+    const tasks = await api.getTasks();
+    const taskUpdate = tasks.filter((v: any) => v.id !== +id);
+    await api.writeTasks(taskUpdate);
+  },
+
+  changeStatus: async (id: string, status: status) => {
+    const tasks = await api.getTasks();
+    const indexTask = tasks.findIndex((v: any) => v.id === +id);
+    const newTasks = tasks.with(indexTask, { ...tasks[indexTask], status });
+    await api.writeTasks(newTasks);
+    console.log(`status correctamente actualizado en id : ${id}`);
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { Command } from 'commander';
+import { api } from './config/api';
+import 'dotenv/config';
+const program = new Command();
+export enum status {
+  inProgress = 'in-progress',
+  todo = 'todo',
+  done = 'done',
+}
+
+program.version('1.0.0').description('A simple todo list');
+
+program.command('list').action(async () => {
+  const tasks = await api.getTasks();
+  console.log(JSON.stringify(tasks, null, 2));
+});
+
+program.command('add <task>').action(async (task) => {
+  try {
+    await api.addTask(task);
+    console.log('Task agregado correctamente');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+program.command('update <id> <text>').action(async (id, task) => {
+  try {
+    await api.updateTask(id, task);
+    console.log(`id: ${id} Correctamente Actualizado`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+program.command('delete <id>').action(async (id: string) => {
+  try {
+    await api.deleteTask(id);
+    console.log(`id: ${id} Correctamente eliminado`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+program.command('mark-in-progress <id>').action(async (id) => {
+  await api.changeStatus(id, status.inProgress);
+});
+program.command('mark-done <id>').action(async (id) => {
+  await api.changeStatus(id, status.done);
+});
+
+program.parse(process.argv);
